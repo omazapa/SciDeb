a436d9a8c2cb2e5e61f5681090956a1d5bf7a58c
Omar Andres Zapata Mesa <andresete.chaos@gmail.com>
2014-01-10 00:49:33 -0500

    configuración inicial, el script config.sh tiene el llamado a live debian para la creación de la carpeta config. -> crea solo la distro live sin instalador -> tiene la imagen de grub -> es solo el sistema base sin escritorio.

---
+++ linux/config.sh
@@ -0,0 +1,17 @@
+lb config \
+--architectures amd64 \
+--distribution testing \
+--archive-areas "main contrib non-free" \
+--mirror-bootstrap http://gfif.udea.edu.co/debian \
+--mirror-binary http://gfif.udea.edu.co/debian \
+--mirror-chroot http://gfif.udea.edu.co/debian \
+--mirror-binary-security http://gfif.udea.edu.co/debian-security \
+--mirror-chroot-security http://gfif.udea.edu.co/debian-security \
+--mirror-debian-installer http://gfif.udea.edu.co/debian \
+--parent-mirror-bootstrap http://gfif.udea.edu.co/debian \
+--parent-mirror-binary http://gfif.udea.edu.co/debian \
+--parent-mirror-chroot http://gfif.udea.edu.co/debian \
+--parent-mirror-binary-security http://gfif.udea.edu.co/debian-security \
+--parent-mirror-chroot-security http://gfif.udea.edu.co/debian-security \
+--apt-source-archives false 
+
