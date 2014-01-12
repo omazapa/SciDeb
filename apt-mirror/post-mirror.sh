0ff4bb4f16251c332c9db3eca38836f011590bbd
Omar Andres Zapata Mesa <andresete.chaos@gmail.com>
2014-01-11 22:09:15 -0500

    optimizado el script post-mirror.sh

--- apt-mirror/post-mirror.sh
+++ apt-mirror/post-mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mirror_path=$(cat /etc/apt/mirror.list | grep mirror_path | gawk -F" " '{print $3}')
+mirror_path=$(cat /etc/apt/mirror.list | grep mirror_path | gawk -F" " '{print $3}' | head -n 1)
 
 ##############################################################
 #Descargar archivos conctens de amd64 para stable/testing/sid#
@@ -8,35 +8,26 @@ mirror_path=$(cat /etc/apt/mirror.list | grep mirror_path | gawk -F" " '{print $
 ########
 #Stable#
 ########
-wget http://ftp.us.debian.org/debian/dists/stable/main/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/stable/main/
+wget -N  http://ftp.us.debian.org/debian/dists/stable/main/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/stable/main/
 
-wget http://ftp.us.debian.org/debian/dists/stable/contrib/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/stable/contrib/
+wget -N  http://ftp.us.debian.org/debian/dists/stable/contrib/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/stable/contrib/
 
-wget http://ftp.us.debian.org/debian/dists/stable/non-free/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/stable/non-free/
+wget -N  http://ftp.us.debian.org/debian/dists/stable/non-free/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/stable/non-free/
 
 #########
 #Testing#
 #########
-wget http://ftp.us.debian.org/debian/dists/testing/main/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/testing/main/
+wget -N  http://ftp.us.debian.org/debian/dists/testing/main/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/testing/main/
 
-wget http://ftp.us.debian.org/debian/dists/testing/contrib/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/testing/contrib/
+wget -N  http://ftp.us.debian.org/debian/dists/testing/contrib/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/testing/contrib/
 
-wget http://ftp.us.debian.org/debian/dists/testing/non-free/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/testing/non-free/
+wget -N  http://ftp.us.debian.org/debian/dists/testing/non-free/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/testing/non-free/
 
 #####
 #Sid#
 #####
-wget http://ftp.us.debian.org/debian/dists/sid/main/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/sid/main/
+wget -N  http://ftp.us.debian.org/debian/dists/sid/main/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/sid/main/
 
-wget http://ftp.us.debian.org/debian/dists/sid/contrib/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/sid/contrib/
+wget -N  http://ftp.us.debian.org/debian/dists/sid/contrib/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/sid/contrib/
 
-wget http://ftp.us.debian.org/debian/dists/sid/non-free/Contents-amd64.gz && \
-mv Contents-amd64.gz $mirror_path/ftp.us.debian.org/debian/dists/sid/non-free/
+wget -N  http://ftp.us.debian.org/debian/dists/sid/non-free/Contents-amd64.gz -P $mirror_path/ftp.us.debian.org/debian/dists/sid/non-free/
