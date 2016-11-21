631e96e1950766f8234a602427e5190525e53ff3
root <root@tuxito.opoject.org>
2016-11-20 22:07:24 -0500

    added package art

--- linux/config.sh
+++ linux/config.sh
@@ -5,26 +5,28 @@ lb config \
 --updates false \
 --security false \
 --archive-areas "main contrib non-free" \
---mirror-bootstrap http://gfif.udea.edu.co/debian \
---mirror-binary http://gfif.udea.edu.co/debian \
---mirror-chroot http://gfif.udea.edu.co/debian \
---mirror-binary-security http://gfif.udea.edu.co/debian-security \
---mirror-chroot-security http://gfif.udea.edu.co/debian-security \
+--mirror-bootstrap http://ftp.us.debian.org/debian \
+--mirror-binary http://ftp.us.debian.org/debian \
+--mirror-chroot http://ftp.us.debian.org/debian \
+--mirror-binary-security http://ftp.us.debian.org/debian-security \
+--mirror-chroot-security http://ftp.us.debian.org/debian-security \
 --parent-mirror-debian-installer http://ftp.us.debian.org/debian \
 --mirror-debian-installer http://ftp.us.debian.org/debian \
 --parent-debian-installer-distribution sid \
 --debian-installer-distribution sid \
---parent-mirror-bootstrap http://gfif.udea.edu.co/debian \
---parent-mirror-binary http://gfif.udea.edu.co/debian \
---parent-mirror-chroot http://gfif.udea.edu.co/debian \
---parent-mirror-binary-security http://gfif.udea.edu.co/debian-security \
---parent-mirror-chroot-security http://gfif.udea.edu.co/debian-security \
+--parent-mirror-bootstrap http://ftp.us.debian.org/debian \
+--parent-mirror-binary http://ftp.us.debian.org/debian \
+--parent-mirror-chroot http://ftp.us.debian.org/debian \
+--parent-mirror-binary-security http://ftp.us.debian.org/debian-security \
+--parent-mirror-chroot-security http://ftp.us.debian.org/debian-security \
 --apt-source-archives false \
 --hdd-label "Gnu/SciDeb" \
 --iso-application "Gnu/SciDeb" \
 --iso-publisher "Gnu/SciDeb Team; http://c3.itm.edu.co/scideb; scideb@c3.itm.edu.co" \
 --apt-indices false \
---debian-installer live
+--debian-installer true \
+--system live \
+--mode debian
 
 
 
